f0037da6cb91c31f8abe9a50770ab8b8189e83e5
Xiao Xiao <xiao@weecology.org>
2016-10-11 19:40:39 -0400

    Pull likelihood functions out into their own. Fix bug.

--- forests_ll.py
+++ forests_ll.py
@@ -21,6 +21,25 @@ def import_raw_data(input_filename):
                          names = ['site', 'sp', 'dbh'], delimiter = ",")
     return data
 
+def log_pssnt(m0, nu, n, M_sp):
+    log_p = -np.log(m0) - np.log(np.log(1 / nu)) + n * np.log(1 - nu) - \
+        sum(np.log(range(1, n + 1))) + (n - 1) * np.log(M_sp / m0) - M_sp / m0
+    return log_p
+
+def log_pssme(lambda1, lambda2, n, M_sp):
+    log_p = np.log(np.exp(lambda1) - 1) + np.log(lambda2) - n * lambda1 - M_sp * lambda2
+    return log_p
+
+def  log_pssnti(m0, nu, n, m_list):
+    log_p = -np.log(np.log(1 / nu)) + n * np.log(1 - nu) - np.log(n) - n * np.log(m0) + \
+        sum(-m_list / m0)
+    return log_p
+
+def log_pssmei(lambda1, lambda2, n, m_list):
+    log_p = np.log(np.exp(lambda1) - 1) + n * np.log(lambda2) - n * lambda1 + \
+        sum(-m_list * lambda2)
+    return log_p
+
 def get_model_lik(comm_dat, min_S = 9):
     """Compute the likelihood for the 4 models (2 neutral, 2 maxent) 
     
@@ -30,34 +49,35 @@ def get_model_lik(comm_dat, min_S = 9):
     """
     sp_list = np.unique(comm_dat['sp'])
     if len(sp_list) > min_S:
+        comm_mass = comm_dat['dbh'] ** 8/3 # convert dbh to mass
         S = len(np.unique(comm_dat['sp']))
         N = len(comm_dat)
-        M = sum(comm_dat['dbh']) # For now M is defind for dbh 
+        M = sum(comm_mass) 
         
         # Parameter estimation
         nu = logser_solver(S, N)
         m0 = M / N
         lambda1 = np.log(N / (N - S))
-        lambda2 = S / M
+        lambda2_ssme = S / M
+        lambda2_ssmei = N / M
         
         # log-likelihoods
         ssnt, ssnti, ssme, ssmei = 0, 0, 0, 0
         for sp in sp_list:
-            m_sp = comm_dat['dbh'][comm_dat['sp'] == sp]
+            m_sp = comm_mass[comm_dat['sp'] == sp]
             n = len(m_sp)
             M_sp = sum(m_sp)
-            ssnt += -np.log(m0) - np.log(-np.log(nu)) + n * np.log(1 - nu) - sum(np.log(range(1, n + 1))) + \
-                (n - 1) * np.log(M_sp / m0) - M_sp / m0
-            ssnti += -np.log(-np.log(nu)) + n * np.log(1 - nu) - np.log(n) - n * np.log(m0) - M_sp / m0
-            ssme += np.log(N / (N - S) - 1) + np.log(lambda2) - n * lambda1 - M_sp * lambda2
-            ssmei += np.log(N / (N - S) - 1) + n * np.log(lambda2) - n * lambda1 - lambda2 * M_sp
+            ssnt += log_pssnt(m0, nu, n, M_sp)
+            ssnti += log_pssnti(m0, nu, n, m_sp)
+            ssme += log_pssme(lambda1, lambda2_ssme, n, M_sp)
+            ssmei += log_pssmei(lambda1, lambda2_ssmei, n, m_sp)
             
         return [ssnt, ssnti, ssme, ssmei]
     
 dat_dir = 'C:\\Users\\Xiao\\Dropbox\\projects\\mete_neutral_comp\\data\\'
-dat_list = ['ACA', 'BCI', 'BVSF', 'CSIRO', 'FERP', 'Lahei', 'LaSelva', 'NC', 'Oosting', 'Serimbu', 
-            'WesternGhats', 'Cocoli', 'Luquillo', 'Sherman', 'Shirakami']
-out_dir = 'C:\\Users\\Xiao\\Dropbox\\projects\\maxent_null\\4model_ll.csv'
+dat_list = ['ACA', 'BCI', 'BVSF', 'CSIRO', 'FERP', 'LaSelva', 'NC', 'Oosting', 'Serimbu', 
+            'WesternGhats', 'Cocoli', 'Luquillo', 'Sherman']
+out_dir = 'C:\\Users\\Xiao\\Dropbox\\projects\\maxent_null\\4model_ll_updated.csv'
 writer_link = open(out_dir, 'wb')
 writer = csv.writer(writer_link)
 writer.writerow(['data', 'site', 'SSNT', 'SSNTI', 'SSME', 'SSMEI'])
@@ -93,3 +113,24 @@ for sp in np.unique(dat_bci['sp']):
     writer.writerow([sp, n, M, ll_ssnt, ll_exp])
 
 writer_link.close()
+
+# This is a sanity check
+# The difference between SSNTI and SSMEI should just be the difference from SAD
+def diff_sad(comm_dat):
+    S = len(np.unique(comm_dat['sp']))
+    N = len(comm_dat)
+    nu = logser_solver(S, N)
+    lambda1 = np.log(N / (N - S))
+    lsad_ssnti, lsad_ssmei = 0, 0
+    for sp in np.unique(comm_dat['sp']):
+        n = len(comm_dat[comm_dat['sp'] == sp])
+        lsad_ssnti += -np.log(n) - np.log(np.log(1 / nu)) + n * np.log(1 - nu)
+        lsad_ssmei += np.log(np.exp(lambda1) - 1) - n * lambda1
+    return lsad_ssnti - lsad_ssmei
+
+for dat_name in dat_list:
+    dat = import_raw_data(dat_dir + dat_name + '.csv')
+    for site in np.unique(dat['site']):
+        site_dat = dat[dat['site'] == site]
+        print dat_name, site, diff_sad(site_dat)
+        
